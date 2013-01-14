86de8ac6b5a3b152467cca9202cb6288cb461cae
jp-silva <zepedro.cs@gmail.com>
2013-01-14 17:52:06 +0000

    initial commit

---
+++ ShoppingList/ShoppingList/ShoppingListAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ShoppingListAppDelegate.h
+//  ShoppingList
+//
+//  Created by pedro on 1/14/13.
+//  Copyright (c) 2013 jpsilva's company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShoppingListAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShoppingList/ShoppingList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShoppingList
+//
+//  Created by pedro on 1/14/13.
+//  Copyright (c) 2013 jpsilva's company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ShoppingListAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ShoppingListAppDelegate class]));
+    }
+}
